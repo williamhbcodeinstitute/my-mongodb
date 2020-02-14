6cf7cd2b80c0e350530fcd384f5c9e79f80ccb88
williamhbcodeinstitute <william.h.barnes@outlook.com>
2020-02-14 17:17:04 +0000

    Updated multiple files to the database

--- mongo.py
+++ mongo.py
@@ -22,8 +22,9 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-new_doc = {'first': 'douglas', 'last': 'adams', 'dob': '11/03/1952', 'hair_colour': 'grey', 'occupation': 'writer', 'nationality': 'english'}
-coll.insert(new_doc)
+new_docs = [{'first': 'terry', 'last': 'pratchett', 'gender': 'm', 'dob': '28/04/1948', 'hair_colour': 'brown', 'occupation': 'writer', 'nationality': 'english'},
+{'first': 'george', 'last': 'may', 'gender': 'm', 'dob': '11/09/1991', 'hair_colour': 'brown', 'occupation': 'Home', 'nationality': 'english'}]
+coll.insert_many(new_docs)
 
 documents = coll.find()
 
